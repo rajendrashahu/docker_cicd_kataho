7bd6670a34ba7611293460fce3f2414db1cfdb6e
Rajendra Shahu <rjn.s2009@gmail.com>
2024-06-10 16:59:25 +0545

    updated

--- cicd_kataho/public/index.php
+++ cicd_kataho/public/index.php
@@ -1,1 +1,6 @@
-<?php echo 'Hello from cicd_kataho'; ?>
+<?php echo 'Hello from cicd_kataho php file load'; ?>
+<hr>
+<h1>Hello CICD 12345</h1>
+    cicd kataho test 1 pipeline test again test again
+    <p>hello world</p>
+    <p>hello world cicd kataho</p>
